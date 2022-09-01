54cf7b0d79ad98b2ddbee322f6d6575c461f9842
kevin866 <k.wang1502483@gmail.com>
2022-08-31 20:34:59 -0700

    Update main.py

--- ml_new/main.py
+++ ml_new/main.py
@@ -2,5 +2,5 @@ from rec.recomd_algo import recommend
 from rec.recomd_algo import rank_all
 import time
 start_time = time.time()
-rank_all()
+recommend(1)
 print(f"main--- {time.time() - start_time} seconds ---")
